1baffad34ae975e7a71b641ad5cfdd36f29a2674
mavasquezcOffgit <85720831+mavasquezcOffgit@users.noreply.github.com>
2021-07-20 14:52:47 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello {
 
     public static void main(String[] args) {
 
-      System.out.println("This is a java app created using Docker example for Marco V");
+      System.out.println("This is a java app created using Docker example for Marco Vasquez");
 
     }
 
